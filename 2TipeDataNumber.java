bed142e7c2ca26631714ec28737f5536ee24bac5
alphawxyz <105125029+alphawxyz@users.noreply.github.com>
2022-12-16 23:45:33 +0700

    Create 2TipeDataNumber.java

---
+++ 2TipeDataNumber.java
@@ -0,0 +1,20 @@
+public class TipeDataNumber {
+    public static void main(String[] args) {
+        byte iniByte = 120;
+        short iniShort = - 20000;
+        int iniInt = 1000000000;
+        long iniLong = 2000000000;
+        long iniLong2 = 2000000000L;
+
+        float iniFloat = 10.10F;
+        double iniDouble = 10.10;
+
+        int decimalInt = 25;
+        int hexaDecimal = 0xFFFFF;
+        int binaryDecimal = 0b10101010;
+
+        int sum = 1000_000_000;
+        long balance = 1000_000_000_000L;
+
+    }
+}
