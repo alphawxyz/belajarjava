09ee6c2b895c903e32ee812d51f06679b99c874b
alphawxyz <105125029+alphawxyz@users.noreply.github.com>
2022-12-16 23:50:49 +0700

    Create 06TipeDataString.java

---
+++ 06TipeDataString.java
@@ -0,0 +1,12 @@
+public class TipeDataString {
+    public static void main(String[] args) {
+
+        String fistname = "Adi";
+        String lastname = "Kurniawan";
+        String fullname = fistname+ " " +lastname;
+
+        System.out.println(fistname);
+        System.out.println(lastname);
+        System.out.println(fullname);
+    }
+}
