0fe3321308c3123b2edee74be68cd5e3796573d5
alphawxyz <105125029+alphawxyz@users.noreply.github.com>
2022-12-18 23:21:10 +0700

    Create 13IfStatement.java

---
+++ 13IfStatement.java
@@ -0,0 +1,11 @@
+public class IfStatement {
+    public static void main(String[] args) {
+
+        var nilai = 80;
+        var absen = 85;
+
+        if(nilai >= 75 && absen >=75){
+            System.out.println("Lulus");
+        }
+    }
+}
